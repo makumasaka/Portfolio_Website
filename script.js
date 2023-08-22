27247081104de937b52865097157b3df7e2efa56
makumasaka <63078396+makumasaka@users.noreply.github.com>
2023-08-21 22:21:54 -0700

    changes to js and html for filter

--- script.js
+++ script.js
@@ -1,35 +1,20 @@
 document.addEventListener('DOMContentLoaded', function () {
-    const filterInput = document.getElementById('filterInput');
-    const projects = document.querySelectorAll('.project');
     const buttons = document.querySelectorAll('nav button');
+    const projects = document.querySelectorAll('.project');
   
-    filterInput.addEventListener('input', function () {
-      const filterValue = filterInput.value.toLowerCase();
-  
-      projects.forEach(project => {
-        const projectTags = project.getAttribute('data-tags').toLowerCase();
-  
-        if (projectTags.includes(filterValue) || filterValue === '') {
-          project.style.display = 'block';
-        } else {
-          project.style.display = 'none';
-        }
-      });
-    });
-
     buttons.forEach(button => {
-        button.addEventListener('click', function () {
-          const filterTag = this.getAttribute('data-tag');
-          
-          projects.forEach(project => {
-            const projectTags = project.getAttribute('data-tags').split(' ');
-    
-            if (filterTag === 'xr' || projectTags.includes(filterTag)) {
-              project.style.display = 'block';
-            } else {
-              project.style.display = 'none';
-            }
-          });
+      button.addEventListener('click', function () {
+        const filterTag = this.getAttribute('data-tag');
+        
+        projects.forEach(project => {
+          const projectTags = project.getAttribute('data-tags').split(' ');
+  
+          if (filterTag === 'all' || projectTags.includes(filterTag)) {
+            project.style.display = 'block';
+          } else {
+            project.style.display = 'none';
+          }
         });
       });
     });
+  });

068f2e9dd18baae6ba52e60e3a9666867445cf21
makumasaka <63078396+makumasaka@users.noreply.github.com>
2023-08-21 21:23:12 -0700

    changes to nav bar

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 document.addEventListener('DOMContentLoaded', function () {
     const filterInput = document.getElementById('filterInput');
     const projects = document.querySelectorAll('.project');
+    const buttons = document.querySelectorAll('nav button');
   
     filterInput.addEventListener('input', function () {
       const filterValue = filterInput.value.toLowerCase();
@@ -15,4 +16,18 @@ document.addEventListener('DOMContentLoaded', function () {
         }
       });
     });
+
+    buttons.forEach(button => {
+        button.addEventListener('click', function () {
+          const targetId = this.id.replace('Btn', ''); // Remove "Btn" from button ID
+          const targetElement = document.getElementById(targetId);
+          
+          if (targetElement) {
+            window.scrollTo({
+              top: targetElement.offsetTop,
+              behavior: 'smooth'
+            });
+          }
+        });
+      });
   });

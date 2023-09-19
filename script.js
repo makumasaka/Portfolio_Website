bb2a9c36af2572a932a729ae736ac248bfe17d5b
makumasaka <63078396+makumasaka@users.noreply.github.com>
2023-09-18 21:44:21 -0700

    changes to nav buttons
    changes to nav buttons - background changes color according to number of clicks

--- script.js
+++ script.js
@@ -2,9 +2,23 @@ document.addEventListener('DOMContentLoaded', function () {
     const buttons = document.querySelectorAll('nav button');
     const projects = document.querySelectorAll('.project');
     const navButtons = document.querySelectorAll('nav button');
+
+    // Initialize a variable to keep track of the button state
+    let isClicked = false;
   
     buttons.forEach(button => {
+      // Add a click event listener to the button
       button.addEventListener('click', function () {
+
+        // Toggle the button state
+        isClicked = !isClicked;
+
+        // Change background color of nav button       
+        if (isClicked) {
+          button.style.backgroundColor = 'transparent'; // New background color when clicked
+        } else {
+          button.style.backgroundColor = '#f39c12'; // Default background color
+        }
 
         const filterTag = this.getAttribute('data-tag');
         
@@ -21,7 +35,9 @@ document.addEventListener('DOMContentLoaded', function () {
     });
 
     navButtons.forEach(button => {
+        // Add a click event listener to the button
         button.addEventListener('click', function () {
+          //Add stacking animation
           projects.forEach((project, index) => {
             project.style.animation = `fallingStackAnimation 1s cubic-bezier(0.25, 1, 0.5, 1) ${0.2 * index}s forwards`;
           });

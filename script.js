ad1a822293a47d8f7db14257964fc72372ba9280
makumasaka <63078396+makumasaka@users.noreply.github.com>
2023-10-10 07:02:48 -0700

    attempts to fix data-tag buttons

--- script.js
+++ script.js
@@ -1,23 +1,41 @@
 document.addEventListener('DOMContentLoaded', function () {
     const buttons = document.querySelectorAll('nav button');
     const projects = document.querySelectorAll('.project');
-    const navButtons = document.querySelectorAll('nav button');
+    const changeText = document.querySelector('#test-text');
 
-    // Initialize a variable to keep track of the button state
-    let isClicked = false;
-  
+    // Initialize a variables to keep track of number of data tags with isClicked = true
+    let tagIndex = 0;
+    let tagTotal = 4;
+
     buttons.forEach(button => {
+      
+      // Initialize a variables to keep track of the button state
+      
+      let isClicked = true;
+      tagIndex++;
+
       // Add a click event listener to the button
       button.addEventListener('click', function () {
 
-        // Toggle the button state
-        isClicked = !isClicked;
-
-        // Change background color of nav button       
-        if (isClicked) {
-          button.style.backgroundColor = 'transparent'; // New background color when clicked
-        } else {
+        changeText.textContent = tagIndex + this.id;
+    
+        if (tagIndex === tagTotal) {
+          tagIndex = 1;
+          buttons.forEach(button => button.style.backgroundColor = 'transparent');
           button.style.backgroundColor = '#f39c12'; // Default background color
+        } else {
+
+          // Change background color of nav button       
+          if (button.style.backgroundColor === '#f39c12') {
+            tagIndex--;
+            isClicked = !isClicked; // Toggle the button state
+            button.style.backgroundColor = 'transparent'; // New background color when clicked
+          } else {
+            tagIndex++;
+            isClicked = !isClicked; // Toggle the button state
+            button.style.backgroundColor = '#f39c12'; // Default background color
+          }
+
         }
 
         const filterTag = this.getAttribute('data-tag');
@@ -31,17 +49,13 @@ document.addEventListener('DOMContentLoaded', function () {
             project.style.display = 'none';
           }
         });
+
+        //Add stacking animation
+        projects.forEach((project, index) => {
+          project.style.animation = `fallingStackAnimation 0.5s cubic-bezier(0.25, 1, 0.5, 1)`;
+        }); 
+    
       });
     });
-
-    navButtons.forEach(button => {
-        // Add a click event listener to the button
-        button.addEventListener('click', function () {
-          //Add stacking animation
-          projects.forEach((project, index) => {
-            project.style.animation = `fallingStackAnimation 1s cubic-bezier(0.25, 1, 0.5, 1) ${0.2 * index}s forwards`;
-          });
-        });
-      });
 
   });

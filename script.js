f927239bfe4de731a0f3e050e39133964b55e97b
makumasaka <63078396+makumasaka@users.noreply.github.com>
2023-10-11 23:07:43 -0700

    Update script.js

--- script.js
+++ script.js
@@ -9,29 +9,35 @@ document.addEventListener('DOMContentLoaded', function () {
           const filterTag = this.getAttribute('data-tag');
           const isActive = activeButtons.includes(filterTag);
 
-          if (activeButtons == "all,xr,nasa,configurator") {
-            activeButtons = [];
-            activeButtons.push(filterTag);
-            button.style.backgroundColor = '#f39c12';
-          }else if (isActive){
-            // Button is active, deactivate it
-            const index = activeButtons.indexOf(filterTag);
-            activeButtons.splice(index, 1);
-            button.style.backgroundColor = 'transparent';
+          if (isActive) {
+              // Button is active, deactivate it
+              const index = activeButtons.indexOf(filterTag);
+              activeButtons.splice(index, 1);
+              button.style.backgroundColor = 'transparent';
           } else {
-            // Button is inactive, activate it
-            activeButtons.push(filterTag);
-            button.style.backgroundColor = '#f39c12';
+              // Button is inactive, activate it
+              activeButtons.push(filterTag);
+              button.style.backgroundColor = '#f39c12';
           }
 
           // Update text content
-          changeText.textContent = activeButtons;          
+          changeText.textContent = activeButtons.join(', ');
+
+          // If all buttons are active, deactivate all buttons
+          if (activeButtons.length === buttons.length) {
+              activeButtons = [];
+              buttons.forEach(btn => {
+                  btn.style.backgroundColor = 'transparent';
+              });
+              changeText.textContent = ''; // Clear text content when all buttons are turned off
+              activeButtons.push(filterTag);
+              button.style.backgroundColor = '#f39c12';
+          }
 
           // Toggle project visibility based on active buttons
           projects.forEach(project => {
               const projectTags = project.getAttribute('data-tags').split(' ');
               const isVisible = activeButtons.includes('all') || projectTags.some(tag => activeButtons.includes(tag));
-
               project.style.display = isVisible ? 'block' : 'none';
           });
 
